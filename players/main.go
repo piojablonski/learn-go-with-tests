f167467421cd25994f460811b35e7ca5c2ac34fb
pjablonski <piojablonski@gmail.com>
2023-01-24 12:30:11 +0100

    Use Filesystem store as a repo for main program

--- players/main.go
+++ players/main.go
@@ -1,12 +1,22 @@
 package main
 
-// "players/store/filesystem"
+import (
+	"log"
+	"net/http"
+	"os"
+	"players/httpserver"
+	"players/store/filesystem"
+)
 
 func main() {
-	// http.HandleFunc("/player", httpserver.HandleGetPlayer)
-	// err := http.ListenAndServe(":8080", httpserver.NewPlayerServer(inmemory.NewStore()))
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
+	f, err := os.Create("db")
+	if err != nil {
+		log.Fatalf("cannot open file %q", err)
+	}
+	store := filesystem.NewStore(f)
+	err = http.ListenAndServe(":8080", httpserver.NewPlayerServer(store))
+	if err != nil {
+		log.Fatal(err)
+	}
 
 }
